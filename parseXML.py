2b0ccc801eb4e71f880f3728f68687731f3cd464
Nitin Pendekanti <42867914+nitinpendekanti@users.noreply.github.com>
2021-09-12 11:52:48 -0500

    Update parseXML.py

--- parseXML.py
+++ parseXML.py
@@ -45,12 +45,12 @@ for i in range(len(notesArr)):
 # -*- coding: utf-8 -*-
 # file: main.py
 
-# u2800 - u283F
+
 unibase = ['280', '281', '282', '283']
 uniend = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 ordered_unicodes = [chr(int(''.join(['0x', j, i]),0)) for j in unibase for i in uniend]
 
-# 64 symbols
+
 brailles = ['⠙','⠹','⠝','⠽',    # C1-8
             '⠑','⠱','⠕','⠵',    # D1-8
             '⠋','⠫','⠏','⠯',    # E1-8
@@ -65,7 +65,7 @@ brailles = ['⠙','⠹','⠝','⠽',    # C1-8
             # '⠖','⠶','⠦','⠔','','','⠜','⠈','⠁','⠃','⠉','⠅',
             # '⠇','⠍','','⠥','⠧','⠭','','']
 
-# corresponding unicodes for Braille symbols
+
 unicodes = [u'\u2800',u'\u282e',u'\u2810',u'\u283c',u'\u282b',u'\u2829',u'\u282f',u'\u2804',
             u'\u2837',u'\u283e',u'\u2821',u'\u282c',u'\u2820',u'\u2824',u'\u2828',u'\u280c',
             u'\u2834',u'\u2802',u'\u2806',u'\u2812',u'\u2832',u'\u2822',u'\u2816',u'\u2836',
@@ -76,7 +76,7 @@ unicodes = [u'\u2800',u'\u282e',u'\u2810',u'\u283c',u'\u282b',u'\u2829',u'\u282f
             u'\u282d',u'\u283d',u'\u2835',u'\u282a',u'\u2833',u'\u283b',u'\u2818',u'\u2838']
 
 
-# corresponding bitwise matrix for Braille symbols
+
 matrixcodes = [
     [[0, 0], [0, 0], [0, 0]],[[0, 1], [1, 0], [1, 1]],[[0, 0], [0, 1], [0, 0]],[[0, 1], [0, 1], [1, 1]],
     [[1, 1], [1, 0], [0, 1]],[[1, 1], [0, 0], [0, 1]],[[1, 1], [1, 0], [1, 1]],[[0, 0], [0, 0], [1, 0]],
@@ -96,12 +96,6 @@ matrixcodes = [
     [[1, 0], [1, 1], [0, 1]],[[1, 1], [1, 1], [0, 1]],[[0, 1], [0, 1], [0, 0]],[[0, 1], [0, 1], [0, 1]]
 ]
 
-# corresponding hexcodes for Braille symbols
-hexbase  = ['2', '3', '4', '5']
-hexend   = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-hexcodes = [''.join([j, i]) for j in hexbase for i in hexend]
-
-# corresponding ascii codes for Braille symbols
 asciicodes = [  'C1', "C2", "C4", "C8", 
                 "D1", "D2", "D4", "D8", 
                 "E1", "E2", "E4", "E8", 
@@ -119,48 +113,20 @@ def convert(string, toNotation, fromNotation):
         if string == fromNotation[i]:
             return toNotation[i]
 
-# ascii to braille. currently supporting grade 1 conversion only
-# you should use convert function to find out possible translations for braille code:
-# convert("⠏⠽⠃⠗⠁⠊⠇⠇⠑⠀⠊⠎⠀⠉⠕⠕⠇⠮", meanings, brailles) OR
-# convert("⠏⠽⠃⠗⠁⠊⠇⠇⠑⠀⠊⠎⠀⠉⠕⠕⠇⠮", words, brailles) OR
-# convert("⠏⠽⠃⠗⠁⠊⠇⠇⠑⠀⠊⠎⠀⠉⠕⠕⠇⠮", decodings, brailles)
 def braille(string):
     return ''.join(convert(string, brailles, asciicodes))
 
-def braille2(string):
-    return "Grade 2 conversion not supported yet. you should use convert function to find out translation possibilities for Grade 2."
-
-# braille to ascii
 def ascii(string):
     return ''.join(convert(string, asciicodes, brailles))
 
-# braille to matrix
 def matrix(string):
     return convert(string, matrixcodes, brailles)
 
-# braille to hex
-def hex(string):
-    return convert(string, hexcodes, brailles)
-
-# braille to dot
-def dot(string):
-    return convert(string, dotcodes, brailles)
-
-# helper function for n2braille
-# these functions differs from ascii, matrix, hex and dot by taking
-# an array of items instead of a string
-def convert_list(arr, toNotation, fromNotation):
-    return [toNotation[fromNotation.index(c)] for c in arr]
-
-# matrix to braille
 def matrix2braille(arr):
     return ''.join(convert_list(arr, brailles, matrixcodes))
 
-# hex to braille
 def hex2braille(arr):
     return ''.join(convert_list(arr, brailles, hexcodes))
-
-# dot to braille
 def dot2braille(arr):
     return ''.join(convert_list(arr, brailles, dotcodes))
 
