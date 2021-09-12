de07aba9df27142ba0fa7d435ef3b04c1be65764
EthanCherian <90527850+EthanCherian@users.noreply.github.com>
2021-09-11 21:34:33 -0500

    Added brf-gen functionality to parseXML

--- parseXML.py
+++ parseXML.py
@@ -80,3 +80,59 @@ for i in range(len(notesArr)):
 
 for note in totalNotesArr:
     print(note)
+
+
+# ----------------------------------------------------------
+
+pitch_length_dict = {"C,1": "⠙",
+                    "D,1": "⠑",
+                    "E,1": "⠋",
+                    "F,1": "⠛",
+                    "G,1": "⠓",
+                    "A,1": "⠊",
+                    "B,1": "⠚", #-----
+                    "C,2": "⠹",
+                    "D,2": "⠱",
+                    "E,2": "⠫",
+                    "F,2": "⠻",
+                    "G,2": "⠳",
+                    "A,2": "⠪",
+                    "B,2": "⠺", #-----
+                    "C,4": "⠝",
+                    "D,4": "⠱",
+                    "E,4": "⠏",
+                    "F,4": "⠟",
+                    "G,4": "⠗",
+                    "A,4": "⠎",
+                    "B,4": "⠞", #-----
+                    "C,8": "⠽",
+                    "D,8": "⠵",
+                    "E,8": "⠯",
+                    "F,8": "⠿",
+                    "G,8": "⠷",
+                    "A,8": "⠮",
+                    "B,8": "⠾"}
+
+octave_dict = { 1: "⠈",
+                2: "⠘",
+                3: "⠸",
+                4: "⠐",
+                5: "⠨",
+                6: "⠰",
+                7: "⠠"}
+
+accidental_dict = { 1: "⠩",
+                    0: "",
+                    -1: "⠣"}
+
+for i in range(len(notesArr)-1):
+    note_i = str(totalNotesArr[i][0])
+    interval_i = str(totalNotesArr[i][1])
+    alter_i = str(totalNotesArr[i][2])
+    duration_i = str(totalNotesArr[i][3])
+    pitch_length_i = note_i + "," + duration_i      # gets pitch + rhythm braille
+    print(pitch_length_dict[pitch_length_i])
+    octave_i = octave_dict[alter_i]                 # gets octave braille
+    print(octave_i)
+    accidental_i = accidental_dict[alter_i]         # gets accidental braille
+    print(accidental_i)
